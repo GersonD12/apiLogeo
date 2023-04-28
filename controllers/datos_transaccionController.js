985ddc0224a59cd7d182419ce239da6fdfb75904
gersond12 <gersonperez715@gmail.com>
2023-04-27 19:08:35 -0600

    login

--- controllers/datos_transaccionController.js
+++ controllers/datos_transaccionController.js
@@ -2,9 +2,16 @@
 const Sequelize = require('sequelize');
 const moment = require('moment');
 const axios = require('axios');
-const db = require('../models')
-const datosgenerales = db.usuarios
+const db = require('../models');
+const jwt = require('jsonwebtoken');
+const datosgenerales = db.usuarios;
+const bcrypt = require('bcrypt');
+const { request } = require('express');
+const secretKey = 'Gerthocolor1512';
+const fs = require('fs');
 module.exports = {
+
+        
     async find (req, res) {
         const operaciones = req.body;
         try {
@@ -13,5 +20,51 @@ module.exports = {
             } catch (error) {
                 console.log(error);
         } 
+    },
+
+    async create (req, res) {
+        const operaciones = req.body;
+        try {
+            const salt=bcrypt.genSaltSync(10);
+            const objetocliente={
+                nombre: operaciones.nombre,
+                ciudad: operaciones.ciudad,
+                direccion_entrega: operaciones.direccion_entrega,
+                cod_postal: operaciones.cod_postal,
+                num_pedidos: operaciones.num_pedidos,
+                id_tipo_cliente: operaciones.id_tipo_cliente,
+                contra: bcrypt.hashSync(operaciones.contra,salt)
+            }
+                await datosgenerales.create(objetocliente);
+                res.send("Usuario registrado con mucho exito");
+            } catch (error) {
+                console.log(error);
+        } 
+    },
+
+    async login (req, res) {
+        // Aquí se verificarían las credenciales del usuario (username y password)
+        const username = req.body.nombre;
+        const password = req.body.contra;
+
+        const objeto = await datosgenerales.findOne({
+            where: {
+                nombre:username
+            }
+        });
+        if(objeto){
+            const match = await bcrypt.compare(password,objeto.contra);
+            if(match){
+                const token = jwt.sign({ username }, secretKey, { expiresIn: 600 });
+                res.send("Bienvenido");
+                fs.writeFileSync('archivo.txt',token);
+                
+            }else{
+                res.send("Contraseña incorrecta");
+            }
+            
+        }else{
+            res.send("El usuario no existe");
+        }
     },
 };
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,35 @@
 const { Router } = require('express');
 const jwt = require('jsonwebtoken');
 const router = Router();
+const fs = require('fs');
+const secretKey = 'Gerthocolor1512';
 
+function authenticateToken(req, res, next) {
+    const token = fs.readFileSync('archivo.txt','utf-8');
+    console.log(token);
+  
+    if (!token) {
+      return res.status(401).json({ error: 'Token no provisto' });
+    }
+  
+    jwt.verify(token, secretKey, (err, decoded) => {
+      if (err) {
+        return res.status(403).json({ error: 'Token inválido' });
+      }
+  
+      req.user = decoded.username;
+      next();
+    });
+  }
 // facturas
 const logicalogin = require('../controllers/datos_transaccionController');
 
 //RUTAS
 module.exports = (app) => {
     //Transacciones
-    router.get('/clientes/find', logicalogin.find);
+    router.get('/clientes/find', authenticateToken,logicalogin.find);
+    router.post('/clientes/create', logicalogin.create);
+    router.get('/clientes/login', logicalogin.login);
     app.use('/', router);
 };
+
